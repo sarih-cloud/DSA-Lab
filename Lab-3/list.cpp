f714fcfddd388ecd0643383807b01847bf34a89d
Shahzaib <shahzaibahmad6789@gmail.com>
2025-09-28 12:09:14 +0500

    fix: minor changes to list.cpp
    For efficiency and cleaner code

--- Lab-3/list.cpp
+++ Lab-3/list.cpp
@@ -20,28 +20,34 @@ class LinkedList
         this->tail = NULL;
     }
 
-    void insertAtBeggining(Node* &head, int value) 
+    ~LinkedList()
+    {
+        cout << "[OPERATION] destroying list to free up memory" << endl;
+        this->destroyList();
+    }
+
+    void insertAtBeggining(int value) 
     {
         // Dynamically allocate memory for a new node
         Node* newNode = new Node();
         newNode->data = value;      // Use the passed value
-        newNode->next = head;       // Make its next value point to the old head
-        head = newNode;             // Update the head to the new node we just added
+        newNode->next = this->head; // Make its next value point to the old head
+        this->head = newNode;       // Update the head to the new node we just added
     }
 
-    void insertAtEnd(Node* &head, int value) 
+    void insertAtEnd(int value) 
     {
         Node* newNode = new Node();
         newNode->data = value;
         newNode->next = NULL;       // There is no node after this, adding to end
 
-        if (head == NULL)       // If the List is empty
+        if (this->head == NULL)     // If the List is empty
         {
-            head = newNode;     // Just make the head this Node
+            this->head = newNode;   // Just make the head this Node
         } 
         else 
         {
-            Node* temp = head;
+            Node* temp = this->head;
             while (temp->next != NULL)  // Traverse to the last node
             {
                 temp = temp->next;
@@ -50,19 +56,19 @@ class LinkedList
         }
     }
 
-    void insertAtPosition(Node* &head, int value, int position)
+    void insertAtPosition(int value, int position)
     {
         Node* newNode = new Node();
         newNode->data = value;
         
         if (position == 1)      // Add Node at head
         {
-            newNode->next = head;
-            head = newNode;
+            newNode->next = this->head;
+            this->head = newNode;
             return;
         }
 
-        Node* temp = head;
+        Node* temp = this->head;
         for (int i = 1; i < position - 1 && temp != NULL; i++)
         {           // Traverse to next Node till at location just behind position
             temp = temp->next;
@@ -79,31 +85,32 @@ class LinkedList
         }
     }
 
-    void deleteFromBeggining(Node* &head)
+    void deleteFromBeggining()
     {
-        if (head == NULL)
+        if (this->head == NULL)
         {
             cout << "[ERROR] cannot delete from beginning, list is empty" << endl;
             return;
         }
-        Node* temp = head;      // Save the current head
-        head = head->next;      // Point head to the next node
+        Node* temp = this->head;      // Save the current head
+        this->head = this->head->next;      // Point head to the next node
         delete temp;            // Free memory
     }
 
-    void deleteFromEnd(Node* &head)
+    void deleteFromEnd()
     {
-        if (head == NULL)
+        if (this->head == NULL)
         {
             cout << "[ERROR] cannot delete from end, list is empty" << endl;
-        }
-        if (head->next == NULL)     // If only one node is left
-        {
-            delete head;
-            head = NULL;
             return;
         }
-        Node* temp = head;
+        if (this->head->next == NULL)     // If only one node is left
+        {
+            delete this->head;
+            this->head = NULL;
+            return;
+        }
+        Node* temp = this->head;
         while (temp->next->next != NULL)
         {       // Traverse to the second last node
             temp = temp->next;
@@ -112,44 +119,44 @@ class LinkedList
         temp->next = NULL;      // Update it to point to NULL
     }
 
-    void deleteFromPosition(Node* &head, int position)
+    void deleteFromPosition(int position)
     {
-        if (head == NULL)
+        if (this->head == NULL)
         {
             cout << "[ERROR] cannot delete from Position " << position << ", list is empty" << endl;
             return;
         }
         if (position == 1)      // Delete from first position
         {
-            Node* temp = head;
-            head = head->next;
+            Node* temp = this->head;
+            this->head = this->head->next;
             delete temp;
             return;
         }
-        Node* temp = head;
+        Node* temp = this->head;
         for (int i = 1; i < position - 1 && temp != NULL; i++)
         {
             temp = temp->next;  // Traverse to the node before the one to be deleted
         }
-        if (temp == NULL || temp->next = NULL)
+        if (temp == NULL || temp->next == NULL)
         {
             cout << "[ERROR] cannot delete from position " << position << ", out of bounds" << endl;
             return;
         }
         Node* nodeToDelete = temp->next;    // Store the node to free memory later
-        temp->next = temp->next->next;      // Update the lionk to remove the node
+        temp->next = temp->next->next;      // Update the link to remove the node
         delete nodeToDelete;                // Free up memory
     }
 
-    void displayList(Node* head)
+    void displayList()
     {
-        if (head == NULL)
+        if (this->head == NULL)
         {
             cout << "[ERROR] cannot display, list is empty" << endl;
             return;
         }
 
-        Node* temp = head;
+        Node* temp = this->head;
         while (temp != NULL) 
         {
             cout << temp->data << " -> ";
@@ -158,20 +165,30 @@ class LinkedList
         cout << "NULL" << endl;     // Show the end of the list
     }
 
-    void destroyList(Node* &head) 
+    void destroyList() 
     {
         Node* temp;
-        while (head != NULL)
+        while (this->head != NULL)
         {
-            temp = head;            // Store the head to delete later
-            head = head->next;      // Move forward
+            temp = this->head;            // Store the head to delete later
+            this->head = this->head->next;      // Move forward
             delete temp;            // Delete the stored head
         }
         cout << "[SUCCESS] list destroyed" << endl;
     }
 };
 
-int main()
+int main()      // Test driver for list class
 {
-    
+    // Create a linked list
+    LinkedList* list = new LinkedList();
+
+    // Insert Nodes at head
+    list->insertAtBeggining(7);
+    list->insertAtBeggining(4);
+    list->insertAtBeggining(12);
+    list->insertAtBeggining(8);
+
+    // Verify that the values were added
+    list->displayList();
 }
